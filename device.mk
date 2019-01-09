6b6d972670dbc3a0819ae66c61b5f32d60cd5051
shoudil <stephenli@codeaurora.org>
2019-01-09 17:04:11 +0800

    sanders: Add framework detect modules to PRODUCT_PACKAGES
    Add JNI and libqti_vndfwk_detect module name to product_packages
    to specify module to be installed to device. Here explicitly
    specify both the core and vendor variant in PRODUCT_PACKAGES.
    
    CRs-Fixed: 2372775
    
    Change-Id: Ic64f5ac0cfbbe9e105a6b48b5bc7687c8d23d7e8

--- device.mk
+++ device.mk
@@ -177,6 +177,10 @@ PRODUCT_PACKAGES += \
     libfmjni \
     init.qti.fm.sh
 
+# Fwk-detect
+PRODUCT_PACKAGES += \
+    libqti_vndfwk_detect
+
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
