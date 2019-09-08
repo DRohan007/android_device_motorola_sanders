cb645cf7797baa7e3419f8a6d579cb7103950414
Erfan Abdi <erfangplus@gmail.com>
2019-09-08 15:04:55 +0430

    sanders: Make shim for libcamera.msm8953.so
    P: GraphicBuffer::lock(uint32_t inUsage, void** vaddr);
    Q: GraphicBuffer::lock(uint32_t inUsage, void** vaddr, int32_t* outBytesPerPixel, int32_t* outBytesPerStride);
    Error: cannot locate symbol "_ZN7android13GraphicBuffer4lockEjPPv" referenced by "/vendor/lib/hw/camera.msm8953.so"
    
    Change-Id: I1c1fe66f3f73149ced601eae87b970ce392d50ee
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- device.mk
+++ device.mk
@@ -117,6 +117,7 @@ PRODUCT_PACKAGES += \
     libgui_vendor \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
+    camera_msm8953_shim \
     android.hardware.camera.provider@2.4-impl \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
