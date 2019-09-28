d62c30321dda8d5af601c1ffe85909222e9f02d8
drohan007 <drohan940@gmail.com>
2019-09-28 20:24:51 +0000

    sanders: Build camera.provider@2.4-service

--- device.mk
+++ device.mk
@@ -113,11 +113,14 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     libbson \
+    Launcher3 \
+    Snap \
     Camera2 \
     libgui_vendor \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     camera_msm8953_shim \
+    android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.4-impl \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
