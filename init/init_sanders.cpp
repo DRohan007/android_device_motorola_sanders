d7b9d7f0151e216951d43e130c5f01c8c502a8e8
Jorge Lucas <jorgelucas.sd@gmail.com>
2020-04-07 14:58:09 -0300

    sanders: Update coral fingerprint to april sp
    Signed-off-by: Jorg3Lucas <jorgelucas.sd@gmail.com>

--- init/init_sanders.cpp
+++ init/init_sanders.cpp
@@ -85,7 +85,7 @@ void vendor_load_properties()
 
     // fingerprint
     property_override("ro.build.description", "sanders-user 8.1.0 OPPS28.65-36-11-4 b6557 release-keys");
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ2A.200305.003/6156912:user/release-keys");
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ2A.200405.005/6254899:user/release-keys");
 
     // rmt_storage
     std::string device = GetProperty("ro.boot.device", "");
