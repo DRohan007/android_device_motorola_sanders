0e40c6c79a208f5d72e74c9ef239a8297b7efc0e
drohan940 <drohan940@gmail.com>
2020-09-15 15:12:21 +0000

    sanders[Tmp]: Drop MotoActions

--- device.mk
+++ device.mk
@@ -278,10 +278,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
-# MotoActions
-PRODUCT_PACKAGES += \
-    MotoActions
-
 # Netutils
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.0 \
